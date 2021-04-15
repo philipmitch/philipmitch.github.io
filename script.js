006734dcfa9a8ad59eabf971ee469bc164b29b3b
Philip Mitchell <67108864+philipmitch@users.noreply.github.com>
2021-04-15 05:11:23 -0400

    My first commit
    Testing  layout

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function start(){
 
-  //  $("body").css("background-color", "pink"); 
-    $("#menu a:first").click();
-
-}
-
-$(window).on("load", start);
-
-function loadpage(e){
-
-    e.preventDefault();
-    $("#menu a.active").removeClass("active");
-    $(this).addClass("active");
-
-    var href = $(this).attr("href");
-    $("#content").load(href);
-
-}
-
-$(document).on("click", "#menu a", loadpage);
+    // $("body").css("background-color", "pink");
+ 
+ }
+ 
+ $(window).on("load", start);
+ 
+ 
+ function loadpage(e){
+ 
+     e.preventDefault();
+     $("#menu a.active").removeClass("active");
+     $(this).addClass("active");
+     
+     $("#content").load("about.html");
+ 
+ }
+ 
+ $(document).on("click", "#menu a", loadpage);
