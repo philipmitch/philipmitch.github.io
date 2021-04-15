efe973e0567778d693926d1c4726413a83613822
Philip Mitchell <67108864+philipmitch@users.noreply.github.com>
2021-04-15 05:57:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function start(){
 
-    $("#menu a:first").click();
-
- }
- 
- $(window).on("load", start);
- 
- 
- function loadpage(e){
- 
-     e.preventDefault();
-     $("#menu a.active").removeClass("active");
-     $(this).addClass("active");
-     
-     var href = $(this).attr("href");
-     $("#content").load(href);
- 
- }
- 
- $(document).on("click", "#menu a", loadpage);
+    //  $("body").css("background-color", "pink"); 
+      $("#menu a:first").click();
+  
+  }
+  
+  $(window).on("load", start);
+  
+  function loadpage(e){
+  
+      e.preventDefault();
+      $("#menu a.active").removeClass("active");
+      $(this).addClass("active");
+  
+      var href = $(this).attr("href");
+      $("#content").load(href);
+  
+  }
+  
+  $(document).on("click", "#menu a", loadpage);
